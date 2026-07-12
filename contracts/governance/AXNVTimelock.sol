ad1c673fbd6a140fa1e09b22a2cc1c08e2d6ba1c
Aditya Kumar K <adithya.k8977@gmail.com>
2026-07-12 09:49:41 +0530

    Add AXNVTimelock contract extending TimelockController

---
+++ contracts/governance/AXNVTimelock.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract AXNVTimelock is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address admin
+    )
+        TimelockController(
+            minDelay,
+            proposers,
+            executors,
+            admin
+        )
+    {}
+}
